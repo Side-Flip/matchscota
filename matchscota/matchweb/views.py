ea86a45436877b11096af676489f705608b29db5
Side-Flip <milo_0922@outlook.com>
2024-05-01 17:15:35 -0500

    establecido el cliente para conectar a la base de datos

--- matchscota/matchweb/views.py
+++ matchscota/matchweb/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from db_connection import db
 
 # Create your views here.
 
 def home(request):
     return HttpResponse('<h1>hello world</h1>')
+
+
