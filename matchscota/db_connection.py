b25a2f716a8c41fa417ae63a96f64eee98f19531
Side-Flip <milo_0922@outlook.com>
2024-05-22 21:33:15 -0500

    implemtada la funcionalidad de registro de usuario con la base de datos

--- matchscota/db_connection.py
+++ matchscota/db_connection.py
@@ -3,4 +3,4 @@ import pymongo
 url = 'mongodb+srv://Camilo:Bn1GAcVpDVvovGq9@cluster0.catjgfv.mongodb.net/'
 client = pymongo.MongoClient(url)
 
-db = client['test_django']
+db = client['matchscota']
--- matchscota/matchweb/views.py
+++ matchscota/matchweb/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 from db_connection import db
+import bcrypt
 
 # Create your views here.
 
 def home(request):
     return render(request,'home.html')
 
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            password = form.cleaned_data['password']
+            #Encrypt the password
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            # Store the user in MondoDB
+            users_collection = db['User']
+            users_collection.insert_one({'name':name, 'password':hashed_password})
+            messages.success(request, 'Registration successful')
+            return redirect('register')
+    else:
+        form = RegisterForm()
 
+    return render(request, 'register.html', {'form':form})
