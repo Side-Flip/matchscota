5e0336cd4be6d7ffb84a3c0af6deec7370a52711
Side-Flip <milo_0922@outlook.com>
2024-05-22 22:45:49 -0500

    implementado la vista de login y agregada la vista de cuando un usuario inicia sesion

--- matchscota/matchweb/forms.py
+++ matchscota/matchweb/forms.py
@@ -3,3 +3,7 @@ from django import forms
 class RegisterForm(forms.Form):
     name = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput())
+
+class LoginForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput())
--- matchscota/matchweb/urls.py
+++ matchscota/matchweb/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
