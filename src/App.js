789194c9d564f5c439221e62e9f26015d2ee8bac
Nery Aponte <nerykab@gmail.com>
2024-06-15 12:54:33 -0500

    interface del perfil

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Loguin from './Componentes/Ingreso/Loguin';
 import Mascotas from './Componentes/Registro/Mascotas';
 import Propietarios from './Componentes/Registro/Propietarios';
+import Perfil from './Componentes/Interfaces/Perfil';
 
 function App() {
   return (
     <div className="container">
-      <Mascotas/>
+      <Perfil/>
     </div>
   );
 }
--- src/Componentes/Interfaces/Perfil.js
+++ src/Componentes/Interfaces/Perfil.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Perfil.css';
 
 const Perfil = () => {
   return (
-    <div>
+    <div className='container'>
       
+      <div className='listado'>
+        <h1> ¡Tus Mascotas! </h1>
+        <div className='mascota'>
+          
+        </div>
+      </div>
+
+      <div className='menu'>
+        <button type='chat'>Chats</button>
+        <button type='match'>Match</button>
+        <button type='tus-mascotas'>¡Tus Mascotas!</button>
+      </div>
     </div>
   )
 }
