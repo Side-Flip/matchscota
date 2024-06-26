e3dee00a26a254e5365d0decdee658d1aea72601
Nery Aponte <nerykab@gmail.com>
2024-06-25 20:09:38 -0500

    Complemento de la interface de perfil

--- src/Componentes/Interfaces/Perfil.js
+++ src/Componentes/Interfaces/Perfil.js
@@ -1,22 +1,64 @@
 import React from 'react'
 import './Perfil.css';
+import Logo from'./Logo.png';
 
 const Perfil = () => {
   return (
     <div className='container'>
-      
-      <div className='listado'>
-        <h1> ¡Tus Mascotas! </h1>
-        <div className='mascota'>
+
+      <div className='estado'>
+
+        <div className='busqueda'>
+          
+        </div>
+
+        <div className='usuario'>
+          <div className='logo'>
+            <img className='logo' src={Logo} />
+          </div>
+          
+          <div className='nombre'>
+           <label >José Luis</label>
+          </div>
           
         </div>
       </div>
 
-      <div className='menu'>
-        <button type='chat'>Chats</button>
-        <button type='match'>Match</button>
-        <button type='tus-mascotas'>¡Tus Mascotas!</button>
+      <div className='contenido'>
+
+        <div>
+          <h1>¡Tus Mascotas!</h1>
+          <div className='listado'>
+            
+            <div className='mascota'> </div>
+          </div>
+        </div>
+
+        <div className='menu'>
+          
+          <div className='btn'>
+            <button type='agregar'>Agregar</button>
+          </div>
+          
+          <div className='btn'>
+            <button type='chat'>Chats</button>
+          </div>
+          
+          <div className='btn'>
+            <button type='match'>Match</button>
+          </div>
+          
+          <div className='btn'>
+            <button type='tus-mascotas'>¡Tus Mascotas!</button>
+          </div>
+          
+        </div>
       </div>
+      
+      
+
+      
+
     </div>
   )
 }
